c4908d431a49e1cc576c91accc9ee6e5586702a6
ravishankar <ravishankar@advarisk.com>
2021-06-30 19:37:39 +0530

    test element and test cases added

---
+++ tests/test_parser.py
@@ -0,0 +1,40 @@
+from os.path import abspath
+
+from s_tool.driver import SeleniumDriver
+from s_tool.parser import select_options
+
+
+def test_select():
+    with SeleniumDriver("firefox", headless=True) as obj:
+
+        # Sample File run from local test folder
+        index_file = "tests/index.html"
+        obj.visit(f"file://{abspath(index_file)}")
+
+        # Test dropdown options with element id
+        options_dicts = select_options(obj.element("id", "sel1"))
+        actual_result = {"1": "1", "2": "2", "3": "3", "4": "4"}
+        assert options_dicts == actual_result
+
+        # Test for invalid element
+        options_dicts = select_options(obj.element("id", "select_id"))
+        assert options_dicts == {}
+
+        # Test for disabled dropdown
+        options_dicts = select_options(obj.element("xpath", '//select[@disabled=""]'))
+        assert options_dicts == {
+            "default": "default",
+            "1": "One",
+            "2": "Two",
+            "3": "Three",
+        }
+
+        # Test for swaping keys and values\
+        elem = obj.element("xpath", '//select[@disabled=""]')
+        options_dicts = select_options(elem, swap=True)
+        assert options_dicts == {
+            "default": "default",
+            "One": "1",
+            "Two": "2",
+            "Three": "3",
+        }
