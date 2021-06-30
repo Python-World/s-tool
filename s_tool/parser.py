ea4eaef2d87a9f1aa4e30cf14b3f160a1509e63f
ravishankar <ravishankar@advarisk.com>
2021-06-30 19:35:45 +0530

    #8 project restructure

--- s_tool/parser.py
+++ s_tool/parser.py
@@ -1,5 +1,26 @@
-class Parser(object):
-    """Parse data from webdriver"""
+from s_tool.utils import get_element
 
-    def __init__(self, driver):
-        self.driver = driver
+
+def select_options(element, swap=None, text_exclude=[]):
+    """Return dropdown option in key value pair
+
+    Args:
+        element      : An select element
+        text_exclude : list of values to exclude from result
+
+    Returns:
+        return dict of values and text of select element,
+        return empty dict() if element is not valid or not exists
+    """
+    option_dict = dict()
+    if element and hasattr(element, "tag_name") and element.tag_name == "select":
+        options = get_element(element, "tag_name", "option", many=True)
+        for option in options:
+            func = option.get_attribute
+            text, value = func("text"), func("value")
+            if text not in text_exclude:
+                if swap:
+                    text, value = value, text
+                option_dict[value] = text
+
+    return option_dict
