2e23435c9f47411dbba186070a9bb8c24af4a8e7
Ravishankar Chavare <33047641+chavarera@users.noreply.github.com>
2021-06-30 11:55:18 +0530

    Revert "restructuring project file structure"

--- s_tool/parser.py
+++ s_tool/parser.py
@@ -1,26 +1,5 @@
-from s_tool.utils import get_element
+class Parser(object):
+    """Parse data from webdriver"""
 
-
-def select_options(element, swap=None, text_exclude=[]):
-    """Return dropdown option in key value pair
-
-    Args:
-        element      : An select element
-        text_exclude : list of values to exclude from result
-
-    Returns:
-        return dict of values and text of select element,
-        return empty dict() if element is not valid or not exists
-    """
-    option_dict = dict()
-    if element and hasattr(element, "tag_name") and element.tag_name == "select":
-        options = get_element(element, "tag_name", "option", many=True)
-        for option in options:
-            func = option.get_attribute
-            text, value = func("text"), func("value")
-            if text not in text_exclude:
-                if swap:
-                    text, value = value, text
-                option_dict[value] = text
-
-    return option_dict
+    def __init__(self, driver):
+        self.driver = driver
--- tests/test_webdrivers.py
+++
@@ -1,7 +0,0 @@
-from s_tool.driver import SeleniumDriver
-
-
-def test_firefox_driver():
-    """Test firefox driver with headless"""
-    with SeleniumDriver("firefox", headless=True) as obj:
-        assert "firefox" in obj.browser_list
